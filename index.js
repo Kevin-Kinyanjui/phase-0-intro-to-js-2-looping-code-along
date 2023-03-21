6f35b589e66cf1bb8e9a57de7e3436793c8e08dd
Kevin-Kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-03-21 14:38:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ const names = ["Guadalupe", "Ollie", "Aki"]
 
 function writeCards(names, event) {
     for (let i = 0; i < names.length; i++) {
-           var str = `Thank you, ${names[i]}, for the ${event} gift.`
-           var newArray = str.slice();
-           
-           console.log(newArray)
+           var str = `"Thank you, ${names[i]}, for the wonderful ${event} gift!"`
+           var newArray = [newArray, ...str];
     }
-   // newArray = newArray.push();
     return newArray
 }
-writeCards(names, "surprise")
+console.log(writeCards(names, "surprise"));
 
 
 function countDown(count) {
